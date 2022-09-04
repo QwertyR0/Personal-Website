cd1f88061c4a784a12b35f5d9c3917eb6cd5c71e
QwertyR0 <75887918+QwertyR0@users.noreply.github.com>
2022-09-04 17:43:06 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function avatarClicked(){
+	const logo = document.getElementById("logo");
+	if(logo.src === "https://avatars.githubusercontent.com/u/75887918?v=4") logo.src = "thanksksmall.png";
+	else logo.src = "https://avatars.githubusercontent.com/u/75887918?v=4";
+	return;
+}
+
+//https://discord.gg/axTfQE4NFX
