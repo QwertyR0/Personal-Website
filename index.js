cc2471f359c8ffcf81db3eb41233693de5285177
QwertyR0 <qwerty.rcode@gmail.com>
2022-09-05 02:04:26 +0300

    copying thing 1.2

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 function avatarClicked(){
+	const name = document.getElementById("username");
 	const logo = document.getElementById("logo");
-	if(logo.src === "https://avatars.githubusercontent.com/u/75887918?v=4") logo.src = "thanksksmall.png";
-	else logo.src = "https://avatars.githubusercontent.com/u/75887918?v=4";
+	if(logo.src === "https://avatars.githubusercontent.com/u/75887918?v=4") {
+		logo.src = "thanksksmall.png";
+		name.textContent = "Qwerty.R#9850";
+	} else {
+		logo.src = "https://avatars.githubusercontent.com/u/75887918?v=4";
+		name.textContent = "QwertyR0";
+	}
 	return;
+}
+
+function usernameClicked(){
+	const name = document.getElementById("username");
+	navigator.clipboard.writeText(name.textContent);
+	const nameContent = name.textContent;	
+	name.textContent = "Copied to Clipboard!";
+
+	setTimeout(function(){
+		name.textContent = nameContent;
+	}, 1000);
 }
